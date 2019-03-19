08aeca25dbc9c63c246093934506ff39f2ea1e4b
iamdevlinph <iamdevlinph@gmail.com>
2019-03-19 18:39:23 +0800

    Add type id

--- src/containers/Fighters/utils/makeData.js
+++ src/containers/Fighters/utils/makeData.js
@@ -2,7 +2,7 @@ import _ from 'lodash';
 
 export default (data, level = 1) => {
   const calculatedData = _.chain(data)
-    .orderBy(['type', 'name'], ['asc', 'asc'])
+    .orderBy(['typeId', 'name'], ['asc', 'asc'])
     .map((val) => {
       const calcStr = val.str + (val.strPerLevel * level);
       const calcAgi = val.agi + (val.agiPerLevel * level);
